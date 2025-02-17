2dc3d6ba86f4e7493fdc1a77f0f6ddc2e89b27f2
CODETRIX <codetrix@MacBook-Pro-2.local>
2025-02-17 16:48:54 +0530

    merge

--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import Styles from "../../styles/header.module.css";
+import service1 from "../../assets/images/service-1.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faRectangleList,
+  faSackDollar,
+  faMoneyBillTransfer,
+  faMoneyBillTrendUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 const page = () => {
   return (
@@ -16,56 +24,194 @@ const page = () => {
           />
         </div>
         <div className="w-2/3 bg-gray-50 text-black p-4">
-          <ul className=" flex justify-center items-center  gap-4">
-            <li>Home</li>
-            <li>About</li>
-            <li>Pages</li>
-            <li>Service</li>
-            <li>News</li>
-            <li>Contact</li>
+          <ul className=" flex justify-center items-center  gap-8 cursor-pointer">
+            <li className={Styles.header_nav}>Home</li>
+            <li className={Styles.header_nav}>About</li>
+            <li className={Styles.header_nav}>Pages</li>
+            <li className={Styles.header_nav}>Service</li>
+            <li className={Styles.header_nav}>News</li>
+            <li className={Styles.header_nav}>Contact</li>
           </ul>
         </div>
       </div>
       <div className={Styles.overall_banner}>
-        <div className={Styles.banner}></div>
+        <section className=" relative">
+          <div className={Styles.banner}></div>
+          <div className=" text-white text-[60px] text-center font-bold absolute top-0 left-0 right-0 py-[160px]">
+            Our Services
+          </div>
+        </section>
       </div>
-      <div className="bg-gray-50">
+      <div className="">
         <div className=" text-center py-11">
           <div className="font-bold text-red-500">Feature</div>
           <div className=" font-bold text-4xl">
             Professional And Dedicated <div>Consulting Solutions.</div>
           </div>
         </div>
-        <div className="flex gap-4 px-10">
-          <div className="w-1/3 bg-gray-500 text-black p-4 flex justify-center items-center rounded-md">
-            <div className=" text-center">
-              <div className=" text-white text-lg font-bold">
-                Business planing
+        <div className="flex gap-4 px-[15rem] pb-[100px]">
+          <div className="w-1/3 text-black  flex justify-center items-center rounded-md relative">
+            <div>
+              <Image
+                src={service1}
+                alt="logo"
+                priority // Ensures faster loading
+                className=" rounded-md"
+              />
+            </div>
+
+            <div className=" absolute right-2 bottom-[-80px] rounded-md bg-white p-[20px] shadow-custom-light">
+              <div className=" bg-red-700 rounded-[50%] absolute top-[-44px] left-[-32px] p-[15px] text-white">
+                <div className=" h-[42px] w-[42px]">
+                  <FontAwesomeIcon icon={faRectangleList} />
+                </div>
               </div>
-              <div className=" text-gray-300 font-medium">
+              <div className=" font-bold text-[20px] mb-3 py-3">
+                Strategy & Planning
+              </div>
+              <div className="text-gray-300 font-medium">
                 Amet minim mollit no duis sit{" "}
                 <div>enim aliqua dolor do amet officia.</div>
               </div>
             </div>
           </div>
-          <div className="w-1/3 bg-gray-500 text-black p-4 flex justify-center items-center rounded-md">
-            <div className=" text-center">
-              <div className=" text-white text-lg font-bold">Tax Strategy</div>
-              <div className=" text-gray-300 font-medium">
+          <div className="w-1/3 text-black  flex justify-center items-center rounded-md relative">
+            <div>
+              <Image
+                src={service1}
+                alt="logo"
+                priority // Ensures faster loading
+                className=" rounded-md"
+              />
+            </div>
+
+            <div className=" absolute right-2 bottom-[-80px] rounded-md bg-white p-[20px] shadow-custom-light">
+              <div className=" bg-red-700 rounded-[50%] absolute top-[-44px] left-[-32px] p-[15px] text-white">
+                <div className=" h-[42px] w-[42px]">
+                  <FontAwesomeIcon icon={faRectangleList} />
+                </div>
+              </div>
+              <div className=" font-bold text-[20px] mb-3 py-3">
+                Program Manager
+              </div>
+              <div className="text-gray-300 font-medium">
                 Amet minim mollit no duis sit{" "}
                 <div>enim aliqua dolor do amet officia.</div>
               </div>
             </div>
           </div>
-          <div className="w-1/3 bg-gray-500 text-black p-4 flex justify-center items-center rounded-md">
-            <div className=" text-center">
-              <div className=" text-white text-lg font-bold">
-                Business Intelligence
+          <div className="w-1/3 text-black  flex justify-center items-center rounded-md relative">
+            <div>
+              <Image
+                src={service1}
+                alt="logo"
+                priority // Ensures faster loading
+                className=" rounded-md"
+              />
+            </div>
+
+            <div className=" absolute right-2 bottom-[-80px] rounded-md bg-white p-[20px] shadow-custom-light">
+              <div className=" bg-red-700 rounded-[50%] absolute top-[-44px] left-[-32px] p-[15px] text-white">
+                <div className=" h-[42px] w-[42px]">
+                  <FontAwesomeIcon icon={faSackDollar} />
+                </div>
               </div>
-              <div className=" text-gray-300 font-medium">
+              <div className=" font-bold text-[20px] mb-3 py-3">
+                Tax Management
+              </div>
+              <div className="text-gray-300 font-medium">
                 Amet minim mollit no duis sit{" "}
                 <div>enim aliqua dolor do amet officia.</div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-4 px-[15rem] pb-[100px]">
+          <div className="w-1/3 text-black  flex justify-center items-center rounded-md relative">
+            <div>
+              <Image
+                src={service1}
+                alt="logo"
+                priority // Ensures faster loading
+                className=" rounded-md"
+              />
+            </div>
+
+            <div className=" absolute right-2 bottom-[-80px] rounded-md bg-white p-[20px] shadow-custom-light">
+              <div className=" bg-red-700 rounded-[50%] absolute top-[-44px] left-[-32px] p-[15px] text-white">
+                <div className=" h-[42px] w-[42px]">
+                  <FontAwesomeIcon icon={faMoneyBillTrendUp} />
+                </div>
+              </div>
+              <div className=" font-bold text-[20px] mb-3 py-3">
+                Invetsment Policy
+              </div>
+              <div className="text-gray-300 font-medium">
+                Amet minim mollit no duis sit{" "}
+                <div>enim aliqua dolor do amet officia.</div>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/3 text-black  flex justify-center items-center rounded-md relative">
+            <div>
+              <Image
+                src={service1}
+                alt="logo"
+                priority // Ensures faster loading
+                className=" rounded-md"
+              />
+            </div>
+
+            <div className=" absolute right-2 bottom-[-80px] rounded-md bg-white p-[20px] shadow-custom-light">
+              <div className=" bg-red-700 rounded-[50%] absolute top-[-44px] left-[-32px] p-[15px] text-white">
+                <div className=" h-[42px] w-[42px]">
+                  <FontAwesomeIcon icon={faRectangleList} />
+                </div>
+              </div>
+              <div className=" font-bold text-[20px] mb-3 py-3">
+                Financial Advices
+              </div>
+              <div className="text-gray-300 font-medium">
+                Amet minim mollit no duis sit{" "}
+                <div>enim aliqua dolor do amet officia.</div>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/3 text-black  flex justify-center items-center rounded-md relative">
+            <div>
+              <Image
+                src={service1}
+                alt="logo"
+                priority // Ensures faster loading
+                className=" rounded-md"
+              />
+            </div>
+            <div className=" absolute right-2 bottom-[-80px] rounded-md bg-white p-[20px] shadow-custom-light">
+              <div className=" bg-red-700 rounded-[50%] absolute top-[-44px] left-[-32px] p-[15px] text-white">
+                <div className=" h-[42px] w-[42px]">
+                  <FontAwesomeIcon icon={faRectangleList} />
+                </div>
+              </div>
+              <div className=" font-bold text-[20px] mb-3 py-3">
+                Insurance Strategy
+              </div>
+              <div className="text-gray-300 font-medium">
+                Amet minim mollit no duis sit{" "}
+                <div>enim aliqua dolor do amet officia.</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={`${Styles.banner_2} flex`}>
+          <div className="w-1/2 bg-gray-50 text-black  flex justify-center items-center">
+            <div className={Styles.left_side_reason}></div>
+          </div>
+          <div className="w-1/2 bg-gray-50 text-black">
+            <div className="py-10 pl-[100px]">
+              <div className=" text-lg font-bold text-red-700">
+                Why Choose Codetrix
+              </div>
+              <div className=" text-black text-[24px] font-bold">Reason For Choosing Our Consultancy</div>
             </div>
           </div>
         </div>
