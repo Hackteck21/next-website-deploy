8ae76227dbf3e3affdf16d234ccbdb542bcd7984
CODETRIX <codetrix@MacBook-Pro-2.local>
2025-02-18 18:32:02 +0530

    merge

--- app/components/footer/page.tsx
+++ app/components/footer/page.tsx
@@ -1,28 +1,29 @@
 import Image from "next/image";
 import React from "react";
-import Styles from "../../styles/header.module.css";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="flex bg-gray-50 justify-center items-center">
-        <div className="w-1/3 bg-gray-50 text-black p-4 flex justify-center items-center">
-          <Image
-            src="https://uattfsec.truefinedge.com/image-pub/original.svg"
-            alt="logo"
-            width={200}
-            height={100}
-            priority
-          />
+    <div className=" bg-slate-400 sticky top-0 z-10">
+      <div className="row">
+        <div className="col-md-4 pb-[16px]">
+          <div className=" flex justify-center items-center">
+            <Image
+              src="https://uattfsec.truefinedge.com/image-pub/original.svg"
+              alt="logo"
+              width={200}
+              height={100}
+              priority
+            />
+          </div>
         </div>
-        <div className="w-2/3 bg-gray-50 text-black p-4">
-          <ul className=" flex justify-center items-center  gap-8 cursor-pointer">
-            <li className={Styles.header_nav}>Home</li>
-            <li className={Styles.header_nav}>About</li>
-            <li className={Styles.header_nav}>Pages</li>
-            <li className={Styles.header_nav}>Service</li>
-            <li className={Styles.header_nav}>News</li>
-            <li className={Styles.header_nav}>Contact</li>
+        <div className="col-md-8 flex justify-center items-center">
+          <ul className=" flex  gap-9 pt-[20px]">
+            <li>Home</li>
+            <li>Pages</li>
+            <li>Services</li>
+            <li>Shop</li>
+            <li>News</li>
+            <li>Contact</li>
           </ul>
         </div>
       </div>
